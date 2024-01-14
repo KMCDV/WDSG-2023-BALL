974b8f7674da80e0dc82b57677891029b29071c5
Krzysztof <krzysztof.malinski@cdv.pl>
2024-01-14 17:49:15 +0100

    UI Toolkit editor With basic scene validation

--- Assets/Editor/LevelCreator.cs
+++ Assets/Editor/LevelCreator.cs
@@ -1,8 +1,11 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Levels;
 using UnityEditor;
 using UnityEditor.Build.Content;
 using UnityEditor.SceneManagement;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
@@ -15,6 +18,7 @@ public class LevelCreator : EditorWindow
     private const string LEVEL_CREATE_BUTTON = "level-create-button";
 
     public SceneAsset sceneAssetPrefabForNewLevels;
+    public Coin coinPrefab;
     
     private const string NEW_LEVEL_SO_PATH = "Assets/Resources/Levels/";
     private const string NEW_LEVEL_SCENE_PATH = "Assets/Scenes/Levels/";
@@ -27,6 +31,9 @@ public class LevelCreator : EditorWindow
     private IntegerField _newLevelPointsToEarnIntegerField;
     private Button _createNewLevelButton;
 
+    private VisualElement root;
+    private List<LevelData> levelDatas;
+
     [MenuItem("Window/UI Toolkit/LevelCreator")]
     public static void ShowExample()
     {
@@ -36,18 +43,152 @@ public class LevelCreator : EditorWindow
 
     public void CreateGUI()
     {
+        levelDatas = Resources.LoadAll<LevelData>("Levels").ToList();
         // Each editor window contains a root VisualElement object
-        VisualElement root = rootVisualElement;
+        root = rootVisualElement;
 
         // Instantiate UXML
         VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
         root.Add(labelFromUXML);
         
+        InitLevelCreationSection(root);
+        
+        DropdownField dropdownField = root.Q<DropdownField>("level-select-dropdown");
+        dropdownField.choices = levelDatas.Select(data => data.name).ToList();
+        dropdownField.RegisterValueChangedCallback(Callback);
+    }
+    private void Callback(ChangeEvent<string> p_evt) => SetupLevelToEdit(p_evt.newValue);
+
+    private LevelData selectedLevelData;
+    
+    private void SetupLevelToEdit(string levelDataName)
+    {
+        selectedLevelData = levelDatas.FirstOrDefault(data => data.name == levelDataName);
+        VisualElement visualElement = root.Q<VisualElement>("so-editor");
+        if (selectedLevelData == null) return;
+        
+        visualElement.Clear();
+        VisualElement scriptableInspector = Editor.CreateEditor(selectedLevelData).CreateInspectorGUI();
+        visualElement.Add(scriptableInspector);
+        /*foreach (VisualElement element in scriptableInspector.Children())
+        {
+            if(element is PropertyField propertyField)
+            {
+                propertyField.RegisterValueChangeCallback((x) =>
+                {
+                    visualElement.Add(CreateErrorWithFixButton("Value of SO Changed", "Reload",
+                        () => { SetupLevelToEdit(levelDataName); }));
+                });
+            }
+        }*/
+        
+        if (CheckLevelSceneName(levelDataName, visualElement))
+            return;
+
+        if (CheckLevelAssetIfExists(levelDataName, visualElement))
+            return;
+
+        if (CheckLevelAssetIsInBuildSettings(levelDataName, visualElement))
+            return;
+        
+        //Check Coins
+        Scene scene = EditorSceneManager.OpenScene(NEW_LEVEL_SCENE_PATH + selectedLevelData.SceneName + ".unity");
+        GameObject[] rootGameObjects = scene.GetRootGameObjects();
+        int coinCount = 0;
+        foreach (GameObject rootGameObject in rootGameObjects)
+        {
+            coinCount += rootGameObject.GetComponentsInChildren<Coin>().Length;
+        }
+        if(coinCount < selectedLevelData.PointsToEarn)
+            visualElement.Add(CreateErrorWithFixButton($"Not enough coins in scene! {selectedLevelData.PointsToEarn-coinCount} missing", "Add Coins To Scene", () =>
+            {
+                Scene newScene = EditorSceneManager.OpenScene(NEW_LEVEL_SCENE_PATH + selectedLevelData.SceneName + ".unity");
+                for (int i = 0; i < selectedLevelData.PointsToEarn-coinCount; i++)
+                {
+                    PrefabUtility.InstantiatePrefab(coinPrefab);
+                }
+                EditorSceneManager.SaveScene(newScene);
+                SetupLevelToEdit(levelDataName);
+            }));
+    }
+
+    private bool CheckLevelAssetIsInBuildSettings(string levelDataName, VisualElement visualElement)
+    {
+        if (EditorBuildSettings.scenes.Any(x => x.path.Contains(selectedLevelData.SceneName + ".unity") && x.enabled) ==
+            false)
+        {
+            visualElement.Add(CreateErrorWithFixButton("Scene is not in build settings!", "Fix", () =>
+            {
+                EditorBuildSettingsScene newScene =
+                    new EditorBuildSettingsScene(NEW_LEVEL_SCENE_PATH + selectedLevelData.SceneName + ".unity", true);
+                EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
+                EditorBuildSettings.scenes = scenes.Append(newScene).ToArray();
+                SetupLevelToEdit(levelDataName);
+            }));
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool CheckLevelAssetIfExists(string levelDataName, VisualElement visualElement)
+    {
+        if (AssetDatabase.LoadAssetAtPath<SceneAsset>(NEW_LEVEL_SCENE_PATH + selectedLevelData.SceneName + ".unity") ==
+            null)
+        {
+            visualElement.Add(CreateErrorWithFixButton("Scene is not in Assets!", "Fix", () =>
+            {
+                EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(sceneAssetPrefabForNewLevels));
+                Scene scene = EditorSceneManager.GetActiveScene();
+                string newLevelScenePath = NEW_LEVEL_SCENE_PATH + selectedLevelData.SceneName + ".unity";
+                EditorSceneManager.SaveScene(scene, newLevelScenePath, true);
+                EditorSceneManager.OpenScene(newLevelScenePath);
+                SetupLevelToEdit(levelDataName);
+            }));
+            return true;
+        }
+        return false;
+    }
+
+    private bool CheckLevelSceneName(string levelDataName, VisualElement visualElement)
+    {
+        if (selectedLevelData.SceneName != selectedLevelData.name)
+        {
+            visualElement.Add(CreateErrorWithFixButton("SceneName and ScriptableObject name must be the same!", "Fix", () =>
+            {
+                selectedLevelData.SceneName = selectedLevelData.name;
+                EditorUtility.SetDirty(selectedLevelData);
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+                SetupLevelToEdit(levelDataName);
+            }));
+            return true;
+        }
+
+        return false;
+    }
+
+    private VisualElement CreateErrorWithFixButton(string labelText, string buttonText, Action buttonAction)
+    {
+        VisualElement nameValidation = new VisualElement();
+        nameValidation.style.flexDirection = FlexDirection.Row;
+        Label element = new Label();
+        element.text =  labelText;
+        element.style.color = Color.red;
+        Button fixButton = new Button(buttonAction);
+        fixButton.text = buttonText;
+        nameValidation.Add(element);
+        nameValidation.Add(fixButton);
+        return nameValidation;
+    }
+
+    private void InitLevelCreationSection(VisualElement root)
+    {
         _newLevelNameTextField = root.Q<TextField>(LEVEL_CREATE_NEW_LEVEL_NAME);
         _newLevelDescriptionTextField = root.Q<TextField>(LEVEL_CREATE_NEW_LEVEL_DESCRIPTION);
         _newLevelPointsToEarnIntegerField = root.Q<IntegerField>(LEVEL_CREATE_NEW_LEVEL_POINTS);
         _createNewLevelButton = root.Q<Button>(LEVEL_CREATE_BUTTON);
-        
+
         _createNewLevelButton.clicked += CreateNewLevel;
     }
 
--- Assets/Scripts/Levels/LevelData.cs
+++ Assets/Scripts/Levels/LevelData.cs
@@ -1,5 +1,9 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEditor.UIElements;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 namespace Levels
 {
@@ -10,5 +14,53 @@ namespace Levels
         public string Description;
         public Sprite Image;
         public int PointsToEarn;
+    }
+    
+    [UnityEditor.CustomEditor(typeof(LevelData))]
+    public class LevelDataCuystomEditor : UnityEditor.Editor
+    {
+        public override VisualElement CreateInspectorGUI()
+        {
+            SerializedObject serializedObject = new SerializedObject(target);
+
+            LevelData levelData = (LevelData)target;
+            
+            VisualElement root = new VisualElement();
+                    
+            SerializedProperty sceneNameProperty = serializedObject.FindProperty(nameof(levelData.SceneName));
+            PropertyField propertyField = new PropertyField(sceneNameProperty);
+            propertyField.name = "Scene Name";
+            propertyField.BindProperty(sceneNameProperty);
+            root.Add(propertyField);
+            
+            SerializedProperty descriptionProperty = serializedObject.FindProperty(nameof(levelData.Description));
+            PropertyField element = new PropertyField(descriptionProperty);
+            element.name = "Description";
+            element.BindProperty(descriptionProperty);
+            root.Add(element);
+            
+            SerializedProperty imageProperty = serializedObject.FindProperty(nameof(levelData.Image));
+            PropertyField child = new PropertyField(imageProperty);
+            child.name = "Image";
+            child.BindProperty(imageProperty);
+            root.Add(child);
+            
+            SerializedProperty pointsToEarnProperty = serializedObject.FindProperty(nameof(levelData.PointsToEarn));
+            PropertyField field = new PropertyField(pointsToEarnProperty);
+            field.name = "Points To Earn";
+            field.BindProperty(pointsToEarnProperty);
+            root.Add(field);
+            
+            //ADD CUSTOM BUTTON
+            Button visualElement = new Button(LoadScene) { text = "Open Scene" };
+            root.Add(visualElement);
+            
+            //UXML LOADING
+            /*VisualTreeAsset loadAssetAtPath = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/LevelCreator.uxml");
+            root.Add(loadAssetAtPath.CloneTree());*/
+            return root;
+        }
+
+        private void LoadScene() => EditorSceneManager.OpenScene("Assets/Scenes/Levels/" + ((LevelData)target).SceneName + ".unity");
     }
 }
